01e85e8a82f6e436694ac379c19fa6ab40904109
Uzair <programmeruzair@gmail.com>
2025-11-29 00:24:11 +0530

    Remove morelorem.txt file from the calculator package

---
+++ functions/run_python_file.py
@@ -0,0 +1,38 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path: str, args=[]):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(abs_working_dir, file_path))
+
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File "{file_path}" not found.'
+    
+    if not abs_file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    
+    try:
+        # Build the command: python <file> <args>
+        cmd = ["python", abs_file_path] + [str(a) for a in args]
+        result = subprocess.run(
+            cmd,
+            cwd=abs_working_dir,
+            timeout=30,
+            capture_output=True,
+            text=True  # return strings instead of bytes
+        )
+
+        stdout = result.stdout.strip()
+        stderr = result.stderr.strip()
+
+        final_str = "STDOUT:\n" + (stdout if stdout else "<empty>") + "\n\n" + "STDERR:\n" + (stderr if stderr else "<empty>") + "\n"
+
+        if result.returncode != 0:
+            final_str += f"\nExit Code: {result.returncode}"
+
+        return final_str
+    except Exception as e:
+        return f'Error: Can\'t run the python file: "{file_path}" - {e}'
--- tests.py
+++ tests.py
@@ -1,11 +1,12 @@
 # from AiCodeAgent.functions.get_files_info import get_files_info
 # from functions.get_file_content import get_file_content
-from functions.write_file import write_file
+# from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 def main():
-    # print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(run_python_file("calculator", "lorem.txt"))
     # print("\n\n")
-    print(write_file("calculator", "pkg2/morelorem.txt", "lorem ipsum dolor sit amet"))
+    # print(write_file("calculator", "pkg2/morelorem.txt", "lorem ipsum dolor sit amet"))
     # print("\n\n")
     # print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
     # print("\n\n")
