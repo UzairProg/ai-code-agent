cba567133d984c5af4f78b6e32913925711fd995
Uzair <programmeruzair@gmail.com>
2025-11-28 23:44:55 +0530

    Implement write_file function with error handling and create directory if needed; update tests

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,1 +1,25 @@
 import os
+
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    # print(abs_working_dir, abs_file_path)
+
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    
+    parent_dir = os.path.dirname(abs_file_path)
+    print(parent_dir)
+    if not os.path.isdir(abs_working_dir):
+        try:
+            os.makedirs(parent_dir)
+        except Exception as e:
+            return f'Could not create dir : {parent_dir} - {e}'
+        
+    try:
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+        
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f'There was an error writing to "{file_path}"'
